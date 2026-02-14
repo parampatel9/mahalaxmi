462d93170f229e87f29013d752523be1736f5855
param <parampatel50329@gmail.com>
2026-02-14 11:08:57 +0530

    dist gh chnages

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ import react from '@vitejs/plugin-react-swc';
 const PORT = 3039;
 
 export default defineConfig({
+  base: '/mahalaxmi/',
   plugins: [
     react(),
     checker({
@@ -33,5 +34,4 @@ export default defineConfig({
   },
   server: { port: PORT, host: true },
   preview: { port: PORT, host: true },
-  base: '/mahalaxmi/',
 });
